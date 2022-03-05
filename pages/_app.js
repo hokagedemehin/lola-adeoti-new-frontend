97e02fb9c349b26ddf84952bab87ace32e1b5da7
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-03-05 18:21:41 +0100

    feat: project infrastructure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
+import { QueryClient, QueryClientProvider } from 'react-query';
+const progress = new ProgressBar({
+  size: 4,
+  color: '#2563eb',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+
+Router.events.on('routeChangeComplete', progress.finish);
+
+Router.events.on('routeChangeError', progress.finish);
+
+// process.env.NODE_ENV === 'development';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
