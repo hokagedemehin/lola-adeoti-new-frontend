df9b071ac56a74507bf098a9ab8e8aa0a4e966e8
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-03-16 20:10:42 +0100

    feat: create user anon id

--- utils/context/GlobalData.js
+++ utils/context/GlobalData.js
@@ -1,21 +1,51 @@
 import { useState, useEffect, createContext, useContext } from 'react';
-
+import { nanoid } from 'nanoid';
+import axios from 'axios';
 export const GlobalContext = createContext();
 
 export default function GlobalDataContext({ children }) {
   const [globalCurr, setGlobalCurr] = useState(null);
+  const [userID, setUserID] = useState(null);
 
   useEffect(() => {
     const curr = localStorage.getItem('currency');
-
+    const anonID = localStorage.getItem('lola-userId');
+    const handleUser = async () => {
+      if (anonID) {
+        setUserID(anonID);
+      } else {
+        let id = nanoid();
+        localStorage.setItem('lola-userId', id);
+        setUserID(id);
+        await createNewUser(id);
+      }
+    };
+    handleUser();
     setGlobalCurr(curr);
   }, []);
 
+  const createNewUser = async (id) => {
+    const URL =
+      process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:1337'
+        : 'https://lola-adeoti-new-backend.herokuapp.com';
+    await axios.post(`${URL}/api/anonusers`, {
+      data: {
+        userId: id,
+      },
+    });
+  };
+
   return (
-    <GlobalContext.Provider value={{ globalCurr, setGlobalCurr }}>
+    <GlobalContext.Provider value={{ globalCurr, setGlobalCurr, userID }}>
       {children}
     </GlobalContext.Provider>
   );
 }
 
 export const useGlobal = () => useContext(GlobalContext);
+
+/**
+ * *if a userdoes not have an Id in local storage
+ * *create one for the user
+ */
