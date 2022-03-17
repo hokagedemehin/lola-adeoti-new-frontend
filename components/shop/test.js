ba39c2ddb4260a9a8a72e8bb376743a0a8f2c722
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-03-17 01:53:01 +0100

    fix: check cypress tests

--- components/shop/test.js
+++ components/shop/test.js
@@ -1,61 +1,264 @@
-{
-  /* <LinkBox
-  as='div'
-  className='rounded-lg p-3 transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:shadow-md'
->
-  
-  <div className='relative h-36 w-36 sm:h-52 sm:w-52'>
-    <Image
-      src={elem?.attributes?.image?.data?.attributes?.url}
-      layout='fill'
-      objectFit='cover'
-      placeholder='blur'
-      blurDataURL={
-        elem?.attributes?.image?.data?.attributes?.formats?.small?.url
-      }
-      alt={elem?.attributes?.name}
-      className='transition delay-150 duration-500 ease-in-out hover:scale-110'
-    />
-  </div>
-  
-  <div className='flex'>
-    
-    <div className='flex flex-col'>
-      <Text className='font-semibold sm:text-xl '>
-        {elem?.attributes?.name}
-      </Text>
-      
-      {globalCurr == 'naira' ? (
-        <div className='flex items-center space-x-2'>
-          <Text className='font-semibold sm:text-lg'>
-            &#8358;{elem?.attributes?.nairaSalePrice}
-          </Text>
-          <Text as='s' className='text-sm font-semibold text-gray-400'>
-            &#8358;{elem?.attributes?.nairaPrice}
-          </Text>
+<div className=' py-4 sm:py-6'>
+  <div className='mx-auto max-w-screen-lg px-2 md:px-4'>
+    <div className='mb-4 flex items-center justify-between border-t border-b py-4'>
+      <div className='flex flex-col gap-0.5'>
+        <span className='block font-bold'>Total</span>
+
+        {/* <!-- stars - start --> */}
+        <div className='-ml-1 flex gap-0.5'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6 text-gray-300'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
         </div>
-      ) : (
-        <div className='flex items-center space-x-2'>
-          <Text className='font-semibold sm:text-lg'>
-            &#x24;{elem?.attributes?.dollarSalePrice}
-          </Text>
-          <Text as='s' className='text-sm font-semibold text-gray-400'>
-            &#x24;{elem?.attributes?.dollarPrice}
-          </Text>
+        {/* <!-- stars - end --> */}
+
+        <span className='block text-sm text-gray-500'>Bases on 27 reviews</span>
+      </div>
+
+      <a
+        href='#'
+        className='inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base'
+      >
+        Write a review
+      </a>
+    </div>
+
+    <div className='divide-y'>
+      {/* <!-- review - start --> */}
+      <div className='flex flex-col gap-3 py-4 md:py-8'>
+        <div>
+          <span className='block text-sm font-bold'>John McCulling</span>
+          <span className='block text-sm text-gray-500'>August 28, 2021</span>
         </div>
-      )}
+
+        {/* <!-- stars - start --> */}
+        <div className='-ml-1 flex gap-0.5'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+        </div>
+        {/* <!-- stars - end --> */}
+
+        <p className='text-gray-600'>
+          This is a section of some simple filler text, also known as
+          placeholder text. It shares some characteristics of a real written
+          text but is random or otherwise generated. It may be used to display a
+          sample of fonts or generate text for testing.
+        </p>
+      </div>
+      {/* <!-- review - end --> */}
+
+      {/* <!-- review - start --> */}
+      <div className='flex flex-col gap-3 py-4 md:py-8'>
+        <div>
+          <span className='block text-sm font-bold'>Kate Berg</span>
+          <span className='block text-sm text-gray-500'>July 21, 2021</span>
+        </div>
+
+        {/* <!-- stars - start --> */}
+        <div className='-ml-1 flex gap-0.5'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+        </div>
+        {/* <!-- stars - end --> */}
+
+        <p className='text-gray-600'>
+          This is a section of some simple filler text, also known as
+          placeholder text. It shares some characteristics of a real written
+          text but is random or otherwise generated. It may be used to display a
+          sample of fonts or generate text for testing.
+        </p>
+      </div>
+      {/* <!-- review - end --> */}
+
+      {/* <!-- review - start --> */}
+      <div className='flex flex-col gap-3 py-4 md:py-8'>
+        <div>
+          <span className='block text-sm font-bold'>Greg Jackson</span>
+          <span className='block text-sm text-gray-500'>March 16, 2021</span>
+        </div>
+
+        {/* <!-- stars - start --> */}
+        <div className='-ml-1 flex gap-0.5'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-yellow-400'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-gray-300'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5 text-gray-300'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+          </svg>
+        </div>
+        {/* <!-- stars - end --> */}
+
+        <p className='text-gray-600'>
+          This is a section of some simple filler text, also known as
+          placeholder text. It shares some characteristics of a real written
+          text but is random or otherwise generated. It may be used to display a
+          sample of fonts or generate text for testing.
+        </p>
+      </div>
+      {/* <!-- review - end --> */}
     </div>
   </div>
-  
-  <div className='flex  pt-4'>
-    <Link href={`/product/${elem?.id}/${elem?.attributes?.slug}`} passHref>
-      <LinkOverlay
-        // onClick={() => handleActive('shop')}
-        className='flex items-center justify-center rounded-lg bg-teal-500 px-2 py-1 font-semibold text-white  transition duration-300 ease-in-out hover:bg-teal-700 hover:text-white hover:shadow-md hover:shadow-teal-200  sm:px-4 sm:py-2 '
-      >
-        Select Options
-      </LinkOverlay>
-    </Link>
-  </div>
-</LinkBox>; */
-}
+</div>;
